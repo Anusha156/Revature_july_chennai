2bcdd42ce83ef0431a227120bfb31feb5e4acd6c
anush <anush@LAPTOP-BNFSHRJM>
2024-09-23 16:22:38 +0530

    admin

--- quickcart-admin/src/main/java/com/projects/quickcart/admin/QuickcartAdminApplication.java
+++ quickcart-admin/src/main/java/com/projects/quickcart/admin/QuickcartAdminApplication.java
@@ -2,10 +2,10 @@ package com.projects.quickcart.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class QuickcartAdminApplication {
 
 	public static void main(String[] args) {
